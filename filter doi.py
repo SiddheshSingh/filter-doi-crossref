87049b4d56fceee5c79083ce6799ba82ad7dc803
Siddhesh Singh <siddheshsingh123@gmail.com>
2018-09-27 04:49:30 +0530

    1) Made new logic for search_all_dois function as previous one was forming un-ending loop due to request of cursor value which does not has any value in it. 2) Added helper functions required for new logic.

--- filter doi.py
+++ filter doi.py
@@ -2,7 +2,7 @@ from habanero import Crossref
 cr = Crossref()
 
 #Converts list of values to csv
-def listtocsv(doi,filename): #doi is a list of dictionary with 'DOI' as an index
+def listtocsv(doi,filename='doi_list.csv'): #doi is a list of dictionary with 'DOI' as an index
     import csv
     csvfile = filename
     with open(csvfile, "w") as output:
@@ -15,14 +15,41 @@ def listtocsv(doi,filename): #doi is a list of dictionary with 'DOI' as an index
 def filter_doi(query,cursor="*"):
     return cr.works(query=query,select='DOI',cursor=cursor,cursor_max=20)
 
-#Saves ALL dois from search results to csv file
+#Filters out items,total results and next-cursor value from result
+#Required as the data type of result is any one of list and dictionary
+def give_vals(result):
+    if type(result)==list:k=result[0]['message']
+    else:k=result['message']
+    return {'items':k['items'],'total-results':k['total-results'],'next-cursor':k['next-cursor']}
+
+def permutations(n,val):
+    counter=0
+    while not n==0:
+        if n<val:
+            counter+=1
+            break
+        counter+=1
+        n-=val
+    return counter
+
+#returns list of all dois
 def search_all_dois(query):
-    fir = filter_doi(query)['message']
+    fir = give_vals(filter_doi(query))
     dois=fir['items']
-    curlen=len(dois)
-    while curlen==20: 
-        fir=filter_doi(query,fir['next-cursor'])['message']
-        k=fir['items']
-        dois+=k
-        curlen=len(k)
+    total_results=fir['total-results']
+    total_permutations=permutations(total_results,20)
+    current_cursor_number=2
+    while current_cursor_number<total_permutations: 
+        fir=give_vals(filter_doi(query,fir['next-cursor']))
+        current_cursor_number+=1
+        dois+=fir['items']
     return dois
+
+'''
+The function "search_all_dois(query)" inputs a string query and 
+returns list of all dois which results in that query in format-
+['DOI':unique_doi,'DOI':unique_doi.....].
+
+function "listtocsv(doi,filename='doi_list.csv')" converts list of
+doi in format returned by search_all_dois function to csv.
+'''
